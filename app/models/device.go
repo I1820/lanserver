af15f4dfd0d9724ac64c987a9b5c85a3cda78120
Parham Alvani <parham.alvani@gmail.com>
2018-03-26 13:16:02 +0430

    Adds device name.

--- app/models/device.go
+++ app/models/device.go
@@ -2,6 +2,7 @@ package models
 
 // Device that is connected to LAN gateway
 type Device struct {
+	Name    string
 	DevEUI  int64 // System wide identification
 	DevAddr int   // network wide identification
 }
