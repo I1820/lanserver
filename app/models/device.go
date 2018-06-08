2f71fbf6774fb35bf3d59ad89a52885768d36afc
Parham Alvani <parham.alvani@gmail.com>
2018-06-08 05:23:03 +0430

    Removes redundant json tag.

--- app/models/device.go
+++ app/models/device.go
@@ -8,7 +8,7 @@ type Device struct {
 	DevEUI      int64  // System wide identification
 	DevAddr     net.IP // network wide identification
 	Token       string // Device JWT token
-	Application string `json:",omitempty"`
+	Application string // Application ID
 }
 
 // DeviceProfile that is a profile for many connected device
