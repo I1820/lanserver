6c5ea3aa9efe16d1be5c4a6c58f6f7bb79f784e9
Parham Alvani <parham.alvani@gmail.com>
2018-06-26 06:31:54 +0430

    Removes revel application.

--- app/models/app.go
+++
@@ -1,8 +0,0 @@
-package models
-
-// Application is a collection of devices with specific purpose
-type Application struct {
-	Name        string `json:"name"`
-	ID          string `json:"id,omitempty"`
-	Description string `json:"description"`
-}
