1f6bfb625aa098f1613675a3d8a220a7812127d5
Parham Alvani <parham.alvani@gmail.com>
2018-08-10 13:28:35 +0430

    Removes device test and add tx message.

--- models/messages.go
+++ models/messages.go
@@ -1,7 +1,16 @@
 package models
 
 // RxMessage represents recieved data from a device
+// from lanserver into uplink
 type RxMessage struct {
 	DevEUI string
 	Data   []byte
+}
+
+// TxMessage represents transmitted data to a device
+// from downlink into downlink
+type TxMessage struct {
+	Confirmed bool
+	FPort     int
+	Data      []byte
 }
