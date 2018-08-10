48d5f38bc7f4845767f49be4381a2ee11176b933
Parham Alvani <parham.alvani@gmail.com>
2018-08-10 23:38:27 +0430

    Completes message models.

--- models/messages.go
+++ models/messages.go
@@ -1,16 +1,29 @@
 package models
 
 // RxMessage represents recieved data from a device
-// from lanserver into uplink
+// from lanserver to uplink
 type RxMessage struct {
 	DevEUI string
 	Data   []byte
 }
 
+// LogMessage represents raw data from a device
+// from device to lanserver
+type LogMessage struct {
+	Data  []byte
+	Token string
+}
+
 // TxMessage represents transmitted data to a device
-// from downlink into downlink
+// from downlink to downlink
 type TxMessage struct {
 	Confirmed bool
 	FPort     int
 	Data      []byte
+}
+
+// NotificationMessage represents raw data to a device
+// from lanserver to device
+type NotificationMessage struct {
+	Data []byte
 }
