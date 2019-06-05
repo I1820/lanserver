c1c908ba38191d3052d424d42c3a81b0bd5ab3a3
Parham Alvani <parham.alvani@gmail.com>
2019-06-05 18:14:59 +0430

    feat: Remove IP Field From Device

--- models/device.go
+++ models/device.go
@@ -1,12 +1,9 @@
 package models
 
-import "net"
-
 // Device that is connected by LAN
 type Device struct {
 	Name   string `json:"name"`
 	DevEUI string `json:"devEUI"`
-	IP     net.IP `json:"ip"`
 	Token  string `json:"token"`
 }
 
