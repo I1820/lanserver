2f243cafa9cf675594301d0c8dc8d993d637cb5b
Parham Alvani <parham.alvani@gmail.com>
2020-03-18 01:09:26 +0330

    feat: Add Store Package

---
+++ store/device.go
@@ -0,0 +1,93 @@
+package store
+
+import (
+	"context"
+
+	"github.com/I1820/lanserver/model"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const Collection = "devices"
+
+type Device struct {
+	db *mongo.Database
+}
+
+func (d Device) Get(ctx context.Context) ([]model.Device, error) {
+	var results = make([]model.Device, 0)
+
+	cur, err := d.db.Collection(Collection).Find(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+
+	for cur.Next(ctx) {
+		var result model.Device
+
+		if err := cur.Decode(&result); err != nil {
+			return nil, err
+		}
+
+		results = append(results, result)
+	}
+	if err := cur.Close(ctx); err != nil {
+		return nil, err
+	}
+
+	return results, nil
+}
+
+func (d Device) Show(ctx context.Context, deviceID string) (model.Device, error) {
+	var dev model.Device
+
+	result := d.db.Collection(Collection).FindOne(ctx, bson.M{
+		"deveui": deviceID,
+	})
+
+	if err := result.Decode(&dev); err != nil {
+		return dev, err
+	}
+
+	return dev, nil
+}
+
+func (d Device) Insert(ctx context.Context, dev model.Device) error {
+	if _, err := d.db.Collection(Collection).InsertOne(ctx, dev); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (d Device) Destroy(ctx context.Context, deviceID string) (model.Device, error) {
+	var dev model.Device
+
+	result := d.db.Collection(Collection).FindOneAndDelete(ctx, bson.M{
+		"deveui": deviceID,
+	})
+
+	if err := result.Decode(&dev); err != nil {
+		return dev, err
+	}
+
+	return dev, nil
+}
+
+func (d Device) Update(ctx context.Context, deviceID string, field string, value interface{}) (model.Device, error) {
+	var dev model.Device
+
+	res := d.db.Collection(Collection).FindOneAndUpdate(ctx, bson.M{
+		"deveui": deviceID,
+	}, bson.M{
+		"$set": bson.M{
+			field: value,
+		},
+	}, options.FindOneAndUpdate().SetReturnDocument(options.After))
+
+	if err := res.Decode(&dev); err != nil {
+		return dev, err
+	}
+
+	return dev, nil
+}
