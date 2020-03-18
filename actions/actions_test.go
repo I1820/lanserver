0d8f7ec7ad90a7dbed20b755d362c9a2e133e8fa
Parham Alvani <parham.alvani@gmail.com>
2020-03-18 08:14:45 +0330

    fix: Correct Actions Test

--- actions/actions_test.go
+++ actions/actions_test.go
@@ -1,16 +1,13 @@
 package actions
 
 import (
-	"context"
-	"os"
 	"testing"
-	"time"
 
+	"github.com/I1820/lanserver/config"
+	"github.com/I1820/lanserver/db"
+	"github.com/I1820/lanserver/store"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/suite"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 // LSTestSuite is a test suite for lanserver component APIs.
@@ -21,31 +18,13 @@ type LSTestSuite struct {
 
 // SetupSuite initiates lanserver test suite
 func (suite *LSTestSuite) SetupSuite() {
-	url := os.Getenv("I1820_LANSERVER_DATABASE_URL")
-	if url == "" {
-		url = "mongodb://127.0.0.1:27017"
-	}
+	cfg := config.New()
 
-	// create mongodb connection
-	client, err := mongo.NewClient(options.Client().ApplyURI(url))
-	if err != nil {
-		suite.NoError(err)
-	}
-	// connect to the mongodb (change database here!)
-	ctxc, donec := context.WithTimeout(context.Background(), 10*time.Second)
-	defer donec()
-	if err := client.Connect(ctxc); err != nil {
-		suite.NoError(err)
-	}
-	// is the mongo really there?
-	ctxp, donep := context.WithTimeout(context.Background(), 2*time.Second)
-	defer donep()
-	if err := client.Ping(ctxp, readpref.Primary()); err != nil {
-		suite.NoError(err)
-	}
-	db := client.Database("lanserver")
+	db, err := db.New(cfg.Database)
+	suite.NoError(err)
+	st := store.Device{DB: db}
 
-	suite.engine = App(true, db)
+	suite.engine = App(true, st)
 }
 
 // Let's test lanserver APIs!
